d6cfac1eb49680eacf29c479526d2af546864468
gpeddle <gpeddle@gmail.com>
2023-12-23 08:00:21 -0500

    add barrel file

---
+++ src/index.ts
@@ -0,0 +1,21 @@
+export { 
+    Condition, 
+    EqualCondition,
+    NotEqualCondition,
+    LessThanCondition,
+    LessThanOrEqualCondition,
+    GreaterThanCondition,
+    GreaterThanOrEqualCondition,
+    StartsWithCondition,
+    ContainsCondition,
+    EndsWithCondition,
+    NullCondition,
+    BlankCondition,
+    EmptyCondition,
+    AndCondition,
+    OrCondition,
+    NotCondition,
+} from './condition';
+
+export { ConditionSerializer } from './serializer';
+//export { tokenize } from './tokenizer';
